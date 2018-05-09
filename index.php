0ce4ce1f4bbc324c3241e469a6580b56697bfdb4
KeithSilcock <silcockk@gmail.com>
2018-05-09 12:25:10 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
+
+    <title>PHP User Auth Demo</title>
+</head>
+<body>
+
+<div class="container">
+    <h1 class="center">PHP User Authentication</h1>
+</div>
+
+</body>
+</html>
